64db9ed1616d0304939c8d35bbc22522b8ca6884
Cheran Sam Raj <a-cheransr@SINGTELCDC.COM>
2019-01-23 23:30:26 +0530

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', {
-        from: 'Biju',
-        text: 'Nantha da server pesre',
-        createdAt: 1234
-    });
-
 socket.on('createMessage', (newMessage) => {
     console.log('Biju unaku message', newMessage);
+    io.emit('newMessage', {
+        from: newMessage.from,
+        text: newMessage.text,
+        createdAt: new Date().getTime()
+    })
 });
 
     socket.on('disconnect', ()=> {
