4c15de61b0a3bb332536f86d7608f9fa42c35bbf
Cheran Sam Raj <a-cheransr@SINGTELCDC.COM>
2019-01-23 23:20:23 +0530

    Add new Message and create new message

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Biju',
+        text: 'Nantha da server pesre',
+        createdAt: 1234
+    });
+
+socket.on('createMessage', (newMessage) => {
+    console.log('Biju unaku message', newMessage);
+});
+
     socket.on('disconnect', ()=> {
         console.log('User is disconnected');
     })
